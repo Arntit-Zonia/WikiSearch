287d6773df41bb47dcd825a5d676dd8edcdb4dc5
Arntit Zonia <48775524+Arntit-Zonia@users.noreply.github.com>
2019-10-09 02:27:17 +0100

    Update script.js
    small code refactoring

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-let input = document.querySelector("input");
-let output = document.querySelector("#output");
-let del = document.querySelector("button");
+const input = document.querySelector("input");
+const output = document.querySelector("#output");
 
 input.addEventListener("keypress", function(e) {
-    let xhr = new XMLHttpRequest();
-    if(e.charCode === 13) {
-        let inputTxt = input.value;
-        output.innerHTML = "";
-        let url = `https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=links&list=search&srlimit=5&srsearch=${inputTxt}`;
-        
-        xhr.open("GET", url, true);
+	const xhr = new XMLHttpRequest();
+	if (e.charCode === 13) {
+		const inputTxt = input.value;
+		output.innerHTML = "";
+		const url = `https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=links&list=search&srlimit=5&srsearch=${inputTxt}`;
 
-        xhr.onload = function() {
-            if(this.status === 200) {
-                if(inputTxt === "" || inputTxt === " ") return;
-                let data = JSON.parse(this.responseText);
-                
-                for(let obj in data.query.search) {
-                    output.innerHTML += `<div class="links"><a target="_blank" class="text-danger" href= "https://en.wikipedia.org/wiki/${data.query.search[obj].title.replace(" ", "%20")}">
+		xhr.open("GET", url, true);
+
+		xhr.onload = function() {
+			if (this.status === 200) {
+				if (inputTxt === "" || inputTxt === " ") return;
+				const data = JSON.parse(this.responseText);
+
+				for (let obj in data.query.search) {
+					output.innerHTML += `<div class="links"><a target="_blank" class="text-danger" href= "https://en.wikipedia.org/wiki/${data.query.search[
+						obj
+					].title.replace(" ", "%20")}">
                     <h3>${data.query.search[obj].title}</h3></a>
-                    <p class="url-snippet">${data.query.search[obj].snippet}...</p></div>`
-                }
-            }
-        }
-        xhr.onerror = function() {
-            console.log("ERROR");
-        }
+                    <p class="url-snippet">${
+						data.query.search[obj].snippet
+					}...</p></div>`;
+				}
+			}
+		};
+		xhr.onerror = function() {
+			console.log("ERROR");
+		};
 
-        xhr.send();
-    }
+		xhr.send();
+		input.value = "";
+	}
 });
-
-del.addEventListener("click", () => input.value = "" );
